e5f338f55a04b08694a84438b1d880f253d16eaf
GulshanJha00 <gk4763549@gmail.com>
2024-05-10 13:12:32 +0530

    NIERA BOOTING

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Chatgpt from './Chatgpt/Chatgpt'
+
+const App = () => {
+  return (
+    <>
+    <Chatgpt/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/abc.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import ReactMarkdown from "react-markdown";
+
+function App() {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("Hello there, How can NIERA help you today?");
+  const [generatingAnswer, setGeneratingAnswer] = useState(false);
+
+  async function generateAnswer(e) {
+    setGeneratingAnswer(true);
+    e.preventDefault();
+    setAnswer("Loading your answer... \n It might take upto 10 seconds");
+    try {
+      const response = await axios({
+        url: `${process.env.REACT_APP_SECRET_KEY}`,
+        method: "post",
+        data: {
+          contents: [{ parts: [{ text: question }] }],
+        },
+      });
+
+      setAnswer(
+        response["data"]["candidates"][0]["content"]["parts"][0]["text"]
+      );
+    } catch (error) {
+      console.log(error);
+      setAnswer("Sorry - Something went wrong. Please try again!");
+    }
+
+    setGeneratingAnswer(false);
+    setQuestion("")
+  }
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault(); 
+      generateAnswer(e);
+    }
+  };
+
+
+  return (
+    <>
+
+    <div className="bg-slate-800">
+
+
+      <div className="top">
+        <img
+          className="image1"
+          src="https://wikiwandv2-19431.kxcdn.com/_next/image?url=https://upload.wikimedia.org/wikipedia/en/thumb/f/fd/NIE_University_logo.svg/640px-NIE_University_logo.svg.png&w=640&q=50"
+          alt=""
+          />
+
+        <h1 className="font-bold text-2xl" >NIERA</h1>
+      </div>
+
+
+<hr />
+<br />
+<br />
+<hr />
+
+
+      <div className="w-full md:w-2/3 m-auto  rounded bg-black my-1 text-white h-[450px] overflow-scroll ">
+        <ReactMarkdown className="p-3">{answer}</ReactMarkdown>
+      </div>
+
+
+      <div className="h-screen p-3 bg-slate-800 ">
+        <form
+          onSubmit={generateAnswer}
+          className="w-full md:w-2/3 m-auto text-center rounded py-2 flex gap-4 mt-16 "
+          >
+          <textarea
+            required
+            className="border rounded w-11/12 my-2 min-h-fit p-3 h-16"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            onKeyDown={handleKeyPress}
+            placeholder="Ask anything"
+            ></textarea>
+          <button
+            type="submit"
+            className="bg-blue-500 p-1 rounded-md hover:bg-blue-800 transition-all duration-300 h-16 mt-2 text-white "
+            disabled={generatingAnswer}
+            >
+            Generate answer
+          </button>
+        </form>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
